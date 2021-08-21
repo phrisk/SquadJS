298facd3ef1e768b24ccd8b7aa8629be5b5e9d48
stevieshannon@gmail.com <stevieshannon@gmail.com>
2021-08-21 12:22:12 +0100

    Fixed config

--- squad-server/plugins/discord-teamkill-extra.js
+++ squad-server/plugins/discord-teamkill-extra.js
@@ -1,9 +1,9 @@
 import DiscordBasePlugin from './discord-base-plugin.js';
 
-export default class DiscordKillFeedExtra extends DiscordBasePlugin {
+export default class DiscordTeamkillExtra extends DiscordBasePlugin {
   static get description() {
     return (
-      'The <code>DiscordKillFeed</code> plugin logs all wounds and related information to a Discord channel for ' +
+      'The <code>DiscordTeamkill</code> plugin logs teamkills and related information to a Discord channel for ' +
       'admins to review.'
     );
   }
@@ -37,29 +37,29 @@ export default class DiscordKillFeedExtra extends DiscordBasePlugin {
   constructor(server, options, connectors) {
     super(server, options, connectors);
 
-    this.onWound = this.onWound.bind(this);
+    this.onTeamkill = this.onTeamkill.bind(this);
   }
 
   async mount() {
-    this.server.on('PLAYER_WOUNDED', this.onWound);
+    this.server.on('TEAMKILL', this.onTeamkill);
   }
 
   async unmount() {
-    this.server.removeEventListener('PLAYER_WOUNDED', this.onWound);
+    this.server.removeEventListener('TEAMKILL', this.onTeamkill);
   }
 
-  async onWound(info) {
+  async onTeamkill(info) {
     if (!info.attacker) return;
 
     const fields = [
       {
         name: "Attacker's Name",
-        value: info.attacker?.name,
+        value: info.attacker.name,
         inline: true
       },
       {
         name: "Attacker's SteamID",
-        value: `[${info.attacker?.steamID}](https://steamcommunity.com/profiles/${info.attacker?.steamID})`,
+        value: `[${info.attacker.steamID}](https://steamcommunity.com/profiles/${info.attacker.steamID})`,
         inline: true
       },
       {
@@ -68,12 +68,12 @@ export default class DiscordKillFeedExtra extends DiscordBasePlugin {
       },
       {
         name: "Victim's Name",
-        value: info.victim?.name,
+        value: info.victim.name,
         inline: true
       },
       {
         name: "Victim's SteamID",
-        value: `[${info.victim?.steamID}](https://steamcommunity.com/profiles/${info.victim?.steamID})`,
+        value: `[${info.victim.steamID}](https://steamcommunity.com/profiles/${info.victim.steamID})`,
         inline: true
       }
     ];
@@ -81,22 +81,22 @@ export default class DiscordKillFeedExtra extends DiscordBasePlugin {
     if (!this.options.disableSCBL)
       fields.push({
         name: 'Squad Community Ban List',
-        value: `[Attacker's Bans](https://squad-community-ban-list.com/search/${info.attacker?.steamID})`
+        value: `[Attacker's Bans](https://squad-community-ban-list.com/search/${info.attacker.steamID})`
       });
 
     fields.push({
       name: "Note",
       value: `\`\`\`
 Date:     ${new Date().toISOString()}
-Attacker: ${info.attacker?.name}
-Victim:   ${info.victim?.name}
+Attacker: ${info.attacker.name}
+Victim:   ${info.victim.name}
 Weapon:   ${info.weapon}
 \`\`\``
     })
 
     await this.sendDiscordMessage({
       embed: {
-        title: `${info.attacker?.name}`,
+        title: `${info.attacker.name}`,
         color: this.options.color,
         fields: fields,
         timestamp: info.time.toISOString()
